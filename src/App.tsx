14ce64e7d5f38435dc5ec311cbc0669478df4856
ghilou05 <157740498+ghilou05@users.noreply.github.com>
2026-02-28 18:49:51 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import IntroBanner from "./components/intro_banner"
 import TechIcons from "./components/tech_icons"
 import FeaturedProjects from "./components/featured_projects"
-import { useState } from 'react'
 
 function App() {
   return (
