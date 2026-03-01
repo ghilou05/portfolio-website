a0b4131a9632e89e9525e800006b0345204ae7a5
ghilou05 <ghilesbouaraba@outlook.com>
2026-03-01 17:35:23 +0000

    making tech icons a component in intro banner instead of APP.tsx

--- src/components/intro_banner.tsx
+++ src/components/intro_banner.tsx
@@ -1,26 +1,24 @@
 import React from 'react'
+import TechIcons from './tech_icons';
 
 const intro_banner: React.FC = () => {
     return (
         <div className="intro-banner">
             <div className="intro-banner__text-wrapper">
                 <div className="intro-banner__text-wrapper__headline">
-                    <h2 className="intro-banner__text-wrapper__headline__name">Hi, I'm <span className="blue-text">Ghiles Bouaraba</span>👋</h2>
-                    <div className="intro-banner__text-wrapper__headline__desc1"> Computer Science & Mathematics Student</div>
-                    <div className="intro-banner__text-wrapper__headline__desc2"><span className="blue-text">Developer</span> | Year in Industry</div>
-                </div>
-                <hr/>
-                <div className="intro-banner__text-wrapper__bottom">
-                    <div className="intro-banner__text-wrapper__bottom__text">I build clean, accessible web applications with React, TypeScript, and C#</div>
+                    <h1 className="intro-banner__text-wrapper__headline__name">Hi, I'm
+                        <br /> Ghiles Bouaraba</h1>
+                    <div className="intro-banner__text-wrapper__headline__desc1"> CS & Maths Student | Developer @ Cantarus
+                        <br /> Building the future, through web and mobile apps
+                    </div>
                 </div>
                 <div className="intro-banner__text-wrapper__buttons">
                     <div className="intro-banner__text-wrapper__buttons__button1">View Projects</div>
                     <div className="intro-banner__text-wrapper__buttons__button2">Contact Me</div>
                 </div>
             </div>
-            <div className="intro-banner__graphic">
-                <img src="/laptopgraphic.png"></img>
-            </div>
+            <TechIcons />
+
         </div>
     )
 };
