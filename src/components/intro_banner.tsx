5d6f674ab43d4b91a1fb5197741f22c5ebbfd539
ghilou05 <157740498+ghilou05@users.noreply.github.com>
2026-03-02 19:38:38 +0000

    Update intro_banner.tsx

--- src/components/intro_banner.tsx
+++ src/components/intro_banner.tsx
@@ -8,7 +8,7 @@ const intro_banner: React.FC = () => {
                 <div className="intro-banner__text-wrapper__headline">
                     <h1 className="intro-banner__text-wrapper__headline__name">Hi, I'm
                         <br /> Ghiles Bouaraba</h1>
-                    <div className="intro-banner__text-wrapper__headline__desc1"> CS & Maths Student | Developer @ Cantarus
+                    <div className="intro-banner__text-wrapper__headline__desc1"> CS & Maths Student | Web Developer
                         <br /> Building the future, through web and mobile apps
                     </div>
                 </div>
